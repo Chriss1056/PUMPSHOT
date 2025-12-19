65bcac3d72b5e447393e3c0c5d04af0002940239
Chriss1056 <97809719+Chriss1056@users.noreply.github.com>
2025-12-19 10:11:13 +0100

    Added the automatic assignment of invoice id's to orders via webhook, removed package-lock.json tracking and added the admin order details block extension to the project.

--- app/components/_index/section-1.tsx
+++ app/components/_index/section-1.tsx
@@ -1,3 +1,8 @@
 export default function Index() {
-  return (<p>This is Tab 2&apos;s content</p>);
+  return (
+  <s-stack gap="base">
+    <s-select></s-select>
+    <s-button variant="primary">Load Order</s-button>
+  </s-stack>
+  );
 }
--- app/routes/webhooks.order_created.tsx
+++ app/routes/webhooks.order_created.tsx
@@ -1,10 +1,141 @@
 import type { ActionFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
 
-export const action = async ({ request }: ActionFunctionArgs) => {
-    const { payload, session, topic, shop } = await authenticate.webhook(request);
-    console.log(`Received ${topic} webhook for ${shop}. Session: ${session}`);
-    console.table(`Payload: ${payload}.`);
+const NAMESPACE = "pumpshot";
+const KEY = "invoice_id";
 
-    return new Response();
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { payload, session, admin } = await authenticate.webhook(request);
+    
+  if (!session) {
+    throw Response.json({ ok: true });
+  }
+  let query = null;
+  let response = null;
+  let data = null;
+  let errors = null;
+
+  query = `
+    query GetShopId {
+      shop {
+        id
+      }
+    }
+  `;
+  response = await admin.graphql(query);
+  data = await response.json();
+  const shop_gid = data?.data?.shop?.id;
+  query = null;
+  response = null;
+  data = null;
+
+  query = `
+    query GetInvoiceMetafield($namespace: String!, $key: String!) {
+      shop {
+        metafield(namespace: $namespace, key: $key) {
+          id
+          value
+        }
+      }
+    }
+  `;
+  response = await admin.graphql(query, {
+    variables: { namespace: NAMESPACE, key: KEY },
+  });
+  data = await response.json();
+  const invoice_id = data?.data?.shop?.metafield?.value;
+  query = null;
+  response = null;
+  data = null;
+
+  const order_gid = payload.admin_graphql_api_id;
+
+  query = `
+    mutation SetOrderMetafield($namespace: String!, $key: String!, $orderId: ID!, $value: String!) {
+      metafieldsSet(metafields: [
+        {
+          type: "single_line_text_field"
+          namespace: $namespace
+          key: $key
+          ownerId: $orderId
+          value: $value
+        }
+      ]) {
+        metafields {
+          id
+          namespace
+          key
+          value
+        }
+        userErrors {
+          field
+          message
+          code
+        }
+      }
+    }
+  `;
+  response = await admin.graphql(query, {
+    variables: { namespace: NAMESPACE, key: KEY, orderId: order_gid, value: invoice_id },
+  });
+  data = await response.json();
+  errors = data?.data?.metafieldsSet?.userErrors || [];
+  query = null;
+  response = null;
+  data = null;
+
+  if (errors.length > 0) {
+    return Response.json({ ok: false, errors }, { status: 400 });
+  }
+  errors = null;
+  
+  const year = new Date().getFullYear();
+  const parts = invoice_id.split('-');
+  const validate = parseInt(parts[0]);
+  let value: string = "";
+
+  if (validate !== year) {
+    value = year.toString() + "-00001";
+  } else {
+    const oldVal = parseInt(parts[1]);
+
+    if (isNaN(oldVal)) {
+      throw new Error('Invalid number after hyphen in incommingValue');
+    }
+
+    value = parts[0] + "-" + (oldVal + 1).toString().padStart(5, "0");
+  }
+
+  query = `
+    mutation UpdateInvoiceMetafield($namespace: String!, $key: String!, $value: String!, $id: ID!) {
+      metafieldsSet(metafields: [{
+        namespace: $namespace,
+        key: $key,
+        value: $value,
+        type: "single_line_text_field",
+        ownerId: $id,
+      }]) {
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+
+  response = await admin.graphql(query, {
+    variables: { namespace: NAMESPACE, key: KEY, value: value, id: shop_gid },
+  });
+  data = await response.json();
+  errors = data?.data?.metafieldsSet?.userErrors || [];
+  query = null;
+  response = null;
+  data = null;
+
+  if (errors.length > 0) {
+    return Response.json({ ok: false, errors }, { status: 400 });
+  }
+  errors = null;
+
+  return Response.json({ ok: true });
 };
